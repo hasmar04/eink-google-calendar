48b0913b16d9af12332d6aecb14addcfc2c9b8bb
hasmar04 <harrisonasmar@gmail.com>
2021-07-07 10:14:41 +1000

    Lots of changes to tidy, incorporate timezones and starting to address issue #2

--- main.py
+++ main.py
@@ -36,35 +36,44 @@ def main():
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-    service = build('calendar', 'v3', credentials=creds)
+    gcal = build('calendar', 'v3', credentials=creds)
 
-    dayOffset = -1
+    dayOffset = 0
     calSelect = 5
+    timezone = pytz.timezone("Australia/Brisbane")
     
     def getTime():
         # Get the time at the start and end of the day
-        today = datetime.utcnow().date()+timedelta(days=dayOffset)
-        startOfDay = datetime.combine(today, time()).isoformat() + 'Z'
-        endOfDay = datetime.combine(today, time(23, 59, 59, 999999)).isoformat() + 'Z'
+        day = datetime.utcnow().date()+timedelta(days=dayOffset)
+        startOfDay = datetime.combine(day, time(second=0), tzinfo=timezone).isoformat()
+        endOfDay = datetime.combine(day, time(23, 59, 59), tzinfo=timezone).isoformat()
         return startOfDay, endOfDay
 
     startOfDay, endOfDay = getTime()
+    print(startOfDay)
+    print(endOfDay)
 
     # Gets the calendars from the user's account
-    calList = service.calendarList().list(minAccessRole='writer').execute()
-    # Create a list to save all the calendar IDs to
+    calList = gcal.calendarList().list(minAccessRole='writer').execute()
+    # Create a list to save all the calendar IDs to a list
     ids = list()
     # iterate through the list of calendars and get each ID, append to the list
     for item in calList["items"]:
         ids.append(item["id"])
     #print(ids)
 
-    calEvents = service.events().list(calendarId=ids[calSelect],singleEvents=True,timeMin=startOfDay,timeMax=endOfDay).execute()
+    calEvents = gcal.events().list(calendarId=ids[calSelect],singleEvents=True,timeMin=startOfDay,timeMax=endOfDay).execute()
     events = list()
     for item in calEvents["items"]:
-        events.append(item["summary"])
-    pprint(calEvents["summary"])
-    pprint(events)
+        events.append(item["start"])
+        #if item["start"] == "*date': *":
+            #print('true')
+        print(str(item["summary"])+', '+str(item["start"])+', '+str(item["end"]))
+    #pprint(calEvents["summary"])
+    #pprint(events)
+
+    #for item in events:
+        #pprint(gcal.events().get(calendarId=ids[calSelect],eventId=item).execute()["summary"])
 
 if __name__ == '__main__':
     main()
